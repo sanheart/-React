bf1500e3314b04bd00a1290fd05b6ec448b6cec2
sanheart <wangrui.0630@outlook.com>
2018-06-06 22:39:56 +0800

    Create CreateContact1.js

---
+++ src/CreateContact1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreateContact1 extends Component {
+	render() {
+		return (
+			<div>
+				Create Contact
+			</div>
+		)
+	}
+}
+
+export default CreateContact1
